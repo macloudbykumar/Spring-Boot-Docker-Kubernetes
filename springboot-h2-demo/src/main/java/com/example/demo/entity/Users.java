2d2c46b48127ff07f9ae4ed870b1d7300fd46a61
macloudbykumar <81429447+macloudbykumar@users.noreply.github.com>
2025-11-30 16:19:23 +0530

    Add files via upload

--- springboot-h2-demo/src/main/java/com/example/demo/entity/Users.java
+++ springboot-h2-demo/src/main/java/com/example/demo/entity/Users.java
@@ -14,4 +14,23 @@ public class Users {
 
     private String name;
     private String email;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // getters and setters
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+
 }
+
